4979cae1752c8541871507adae1533ddd62f4c1d
Akashdeep-ofc <akashdeepkanwar1@gmail.com>
2026-05-06 18:22:29 +0530

    Organised a bit

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from backend.app.db.init_db import init_db
-from backend.app.api.v1.routes import auth
 from backend.app.api.v1.routes.user import router
 
 
